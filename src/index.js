11be69ada124b295579ce0c357813cd5ac977165
angel <albarros1973@gmail.com>
2021-02-19 14:30:59 +0100

    Create basic view

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import './css/styles.scss';
+import requests from './requestManager';
+import designer from './view_designer';
+import { fieldsForAddGame, fieldsForSetScore } from './form_data';
+
+const container = designer().addElement(document.querySelector('body'), 'div', null, ['container']);
+const gameContainer = designer().addElement(container, 'div', null, ['container-vert']);
+designer().addElement(gameContainer, 'h2', 'Create a new game', null);
+
+designer().createForm(gameContainer, 'game', 'form', fieldsForAddGame, requests.setGame, null);
+
+const scoreContainer = designer().addElement(container, 'div', null, ['container-vert']);
+designer().addElement(scoreContainer, 'h2', 'Send score', null);
+
+designer().createForm(scoreContainer, 'score', 'form', fieldsForSetScore, requests.setScore, null);
+
+const scoresContainer = designer().addElement(container, 'div', null, ['container-vert']);
+designer().addElement(scoresContainer, 'h2', 'Leaderboard', null);
+const button = designer().addElement(scoresContainer, 'button', 'Get Scores', ['button']);
+
+button.addEventListener('click', () => {
+  requests.getScores();
+});
